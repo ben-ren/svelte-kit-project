fe22a7be49e16a4b1bffe81f9afd4126ed3f6eca
Benjamin Rendell <benjamin.rendell@students.mq.edu.au>
2024-02-29 17:02:16 +1100

    Added transition animations to <TextSwap/>

--- src/routes/TextSwap.svelte
+++ src/routes/TextSwap.svelte
@@ -1,10 +1,22 @@
 <script>
+    import {fade} from 'svelte/transition';
+
     let text = "";
+    let animate = false;
+
+    function handleInputChange(event){
+        animate = !animate;
+        setTimeout(() => {animate = true;}, 250)
+    }
 </script>
 
 <div>
-    <input bind:value={text} placeholder="Enter text here." type="text" id="input_text" name="input_text">
-    <p>{text}</p>
+    <input bind:value={text} on:input={handleInputChange} placeholder="Enter text here." type="text" id="input_text" name="input_text">
+    {#if animate}
+        <p transition:fade >
+            {text}
+        </p>
+    {/if}
 </div>
 
 <style>
